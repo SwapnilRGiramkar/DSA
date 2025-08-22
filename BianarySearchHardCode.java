46d87408608b3b335c8ff81aeef77a89018a4e8e
SwapnilRGiramkar <swapnilrajmira9764@gmail.com>
2025-08-22 22:18:20 +0530

    This is the Binary Search Program

--- BianarySearchHardCode.java
+++ BianarySearchHardCode.java
@@ -7,34 +7,38 @@ public class BinarySearch
 	public static int binary(int[]arr,int search)
 	{
 		int i=0;int j=arr.length-1;
-		for(;i<arr.length-1;i++)
+		while(i<=j)
 		{
-			if(search==arr[i])
+			int mid=(i+(j-i))/2;
+			if(arr[mid]==search)
 			{
-				return i;
+				return mid;
 			}
-			else if(search==arr[j])
+			else if(arr[mid]<search)
 			{
-				return j;
+				i=mid+1;
 			}
 			else
 			{
-				i++;j--;
+				j=mid-1;
 			}
 		}
 		return-1;
 	}
 	public static void main(String[] args)
 	{
-		int []arr= {3,2,4,5,1,6,7,83,67,8,3,6,8};
-		int search=83;
+		int []arr= {1,2,3,4,5,6,7,8,9,10};
+		int search=5;
 		int ans=binary(arr, search);
 		if(ans!=-1)
 		{
-			System.out.println(search+ " Element Found at the Index of "+(ans+1));
+			System.out.println(search+ " Element Found at the Index of "+ans);
 		}
 		else
 			System.out.println("Element Not Found in the Array");
 	}
 }
+
+
+
 
