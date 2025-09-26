4efb5f3ab101636e15783053109dea84d5933551
SwapnilRGiramkar <swapnilrajmira9764@gmail.com>
2025-09-26 11:48:15 +0530

    Given a string of lowercase letters in the range ascii[a-z], determine the index of a character that can be removed to make the string a palindrome. There may be more than one solution, but any will do. If the word is already a palindrome or there is no solution, return -1. Otherwise, return the index of a character to remove.

---
+++ DeletePalHackerRank.java
@@ -0,0 +1,50 @@
+//Delete Palindrome From HackerRank
+package basicDSA;
+
+public class DeletePalindrome 
+{
+	public static void main(String[] args)
+	{
+		String s="aaaabaaa";
+		int ans=delPal(s);
+		System.out.println(ans);
+	}
+	public static int delPal(String s)
+	{
+		s=s.toLowerCase();
+		int i=0,j=s.length()-1;
+		while(i<j)
+		{
+			if(s.charAt(i)==s.charAt(j)) 
+			{
+				i++;j--;
+			}
+			else
+			{
+				if(isPalindrome(s, i+1, j))
+				{
+					return i;
+				}
+				else if(isPalindrome(s, i, j-1))
+				{
+					return j;
+				}
+				else 
+				{
+					return -1;
+				}
+			}
+		}
+		return -1;
+	}
+	public static boolean isPalindrome(String s,int i,int j)
+	{
+		while(i<j)
+		{
+			if(s.charAt(i)!=s.charAt(j)) return false;
+			i++;j--;
+		}
+		return true;
+	}
+}
+
